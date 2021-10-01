20fe4374caf154ae705ebfcfe7f5665ab787f373
nealzhu <30416461+nealzxz@users.noreply.github.com>
2021-10-01 17:50:29 +0300

    Create Day4 RockPaperScissors.py

---
+++ Day4 RockPaperScissors.py
@@ -0,0 +1,38 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+player = input("Type 0 for rock, 1 for paper, 2 for scissors")
+computer = random.randint(0, 2)
+result = 0
+
+if player == "0":
+  if computer == 1:
+    result = 0
+  elif computer == 
+    result = 1
+
+#I will save some time because I knew how to do this
