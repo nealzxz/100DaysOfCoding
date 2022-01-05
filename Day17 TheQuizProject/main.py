b481125a0f596f5112d85065080353a6007c1d75
nealzhu <zxzhu30@gmail.com>
2022-01-05 22:35:37 +0300

    day17 fin

---
+++ Day17 TheQuizProject/main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    question_bank.append(Question(question["text"], question["answer"]))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_have_questions():
+    quiz.next_question()
+
+
+print(f"your final score was : {quiz.score}/{len(question_bank)}")
