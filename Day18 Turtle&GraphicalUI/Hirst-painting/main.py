ec2eb4664e3598b32bf9a2edea84aecc05915c62
nealzhu <zxzhu30@gmail.com>
2022-01-19 22:59:36 +0300

    Day18

--- Day18 Turtle&GraphicalUI/Hirst-painting/main.py
+++ Day18 Turtle&GraphicalUI/Hirst-painting/main.py
@@ -1,10 +1,38 @@
 ###This code will not work in repl.it as there is no access to the colorgram package here.###
 ##We talk about this in the video tutorials##
-import colorgram
+import random
+import turtle as tim
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    rgb_colors.append(color.rgb)
+# import colorgram
+#
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-print(rgb_colors)
+t = tim.Turtle()
+tim.colormode(255)
+
+color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+t.penup()
+t.speed(0)
+s = -200
+for _ in range(10):
+    t.goto(-250, s)
+    for _ in range(10):
+        t.dot(20, random.choice(color_list))
+        t.forward(50)
+    s += 50
+
+
+screen = tim.Screen()
+screen.exitonclick()
+
+
